bfbe3d47ba2ac2197056287bf4a49a1252e845d4
Jossic <jossic.lapierre@gmail.com>
2022-03-04 13:23:12 +0100

    initial commit

---
+++ src/main/java/com/lapidev/springtuto/demo/SpringTutoApplication.java
@@ -0,0 +1,13 @@
+package com.lapidev.springtuto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lapidev/springtuto/demo/SpringTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapidev.springtuto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
